7d217942d1527b2fe4fdbbcc089eee9a34099f61
Sean Curtis <Sean Curtis>
2017-05-06 15:47:44 -0700

    Initial drawing of a "square"
    This successfully draws a "square".

--- app/src/main/java/com/curtis/family/touchexplosion/MyGLRenderer.java
+++ app/src/main/java/com/curtis/family/touchexplosion/MyGLRenderer.java
@@ -10,9 +10,12 @@ import javax.microedition.khronos.opengles.GL10;
  * The OpenGL renderer.
  */
 public class MyGLRenderer implements GLSurfaceView.Renderer {
+    private Particle mParticle;
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        mParticle = new Particle();
     }
 
     public void onSurfaceChanged(GL10 unused, int width, int height) {
@@ -22,5 +25,18 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        mParticle.draw();
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
     }
 }
