b934ab67da378d83213bae483435cdea5a7b2897
Sean Curtis <Sean Curtis>
2017-05-07 07:53:13 -0700

    Move particle drawing logic into ParticleSystem
    It doesn't make sense to pay the gl program cost on a per-particle basis.
    This makes things a big cheaper by moving it into the system draw call so
    it happens once per frame instead of once per particle.
    
    Much faster -- still have much to do.

--- app/src/main/java/com/curtis/family/touchexplosion/SimpleParticleSystem.java
+++ app/src/main/java/com/curtis/family/touchexplosion/SimpleParticleSystem.java
@@ -1,19 +1,25 @@
 package com.curtis.family.touchexplosion;
 
+import android.opengl.GLES20;
 import android.opengl.Matrix;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * Created by Sean on 5/6/2017.
+ * Simple particle system; an explosion of sprites from the tap location that fall to the ground.
  */
 public class SimpleParticleSystem extends ParticleSystem {
 
     private final String vertexShaderCode =
-            "attribute vec4 vPosition;" +
+            "uniform mat4 uMVPMatrix;" +
+                    "attribute vec4 vPosition;" +
                     "void main() {" +
-                    "  gl_Position = vPosition;" +
+                    "  gl_Position = uMVPMatrix * vPosition;" +
                     "}";
 
     private final String fragmentShaderCode =
@@ -23,6 +29,25 @@ public class SimpleParticleSystem extends ParticleSystem {
                     "  gl_FragColor = vColor;" +
                     "}";
 
+    private FloatBuffer vertexBuffer;
+    private ShortBuffer drawListBuffer;
+
+    static final int COORDS_PER_VERTEX = 3;
+    static float squareCoords[] = {
+            -0.5f,  0.5f, 0.0f,   // top left
+            -0.5f, -0.5f, 0.0f,   // bottom left
+            0.5f, 0.5f, 0.0f,   // bottom right
+            0.5f, -0.5f, 0.0f }; // top right
+
+    private final int vertexCount = squareCoords.length / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+
+    // Set color with red, green, blue and alpha (opacity) values
+    float color[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+    private short drawOrder[] = { 0, 1, 2, 0, 2, 3 }; // order to draw vertices
+    private int mProgram;
+
     public SimpleParticleSystem() {
         super();
         mParticles = new ArrayList<SimpleParticle>();
@@ -30,7 +55,6 @@ public class SimpleParticleSystem extends ParticleSystem {
         mVel = new Vector3();
         scratch = new Vector3();
         mMat = new float[16];
-        mParticle = new Particle();
         random = new Random();
         mSync = new Object();
     }
@@ -44,6 +68,40 @@ public class SimpleParticleSystem extends ParticleSystem {
 //        }
 //        mPositionHandle = mGlProgram.addAttributeHandle("vPosition");
 //        mColorHandle = mGlProgram.addUniformHandle("vColor");
+        // initialize vertex byte buffer for shape coordinates
+        ByteBuffer bb = ByteBuffer.allocateDirect(
+                // (# of coordinate values * 4 bytes per float)
+                squareCoords.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(squareCoords);
+        vertexBuffer.position(0);
+
+        // initialize byte buffer for the draw list
+        ByteBuffer dlb = ByteBuffer.allocateDirect(
+                // (# of coordinate values * 2 bytes per short)
+                drawOrder.length * 2);
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
+        drawListBuffer.put(drawOrder);
+        drawListBuffer.position(0);
+
+        int vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
+                vertexShaderCode);
+        int fragmentShader = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
+                fragmentShaderCode);
+
+        // create empty OpenGL ES Program
+        mProgram = GLES20.glCreateProgram();
+
+        // add the vertex shader to program
+        GLES20.glAttachShader(mProgram, vertexShader);
+
+        // add the fragment shader to program
+        GLES20.glAttachShader(mProgram, fragmentShader);
+
+        // creates OpenGL ES program executables
+        GLES20.glLinkProgram(mProgram);
     }
 
     @Override
@@ -51,14 +109,38 @@ public class SimpleParticleSystem extends ParticleSystem {
 //        Quad.preDraw(mGlProgram);
 //        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
 //        Quad.draw();
+
+        // Add program to OpenGL ES environment
+        GLES20.glUseProgram(mProgram);
+
+        // get handle to vertex shader's vPosition member
+        int mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+        // Enable a handle to the triangle vertices
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // Prepare the triangle coordinate data
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+
+        // get handle to fragment shader's vColor member
+        int mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+
+        // Set color for drawing the triangle
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
         synchronized (mSync) {
             for (SimpleParticle particle : mParticles) {
                 drawParticle(particle, globalT, mMVPMatrix);
             }
         }
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
     }
 
-    protected void drawParticle(SimpleParticle particle, long globalT, float[] mMVPMatrix) {
+    protected void drawParticle(SimpleParticle particle, long globalT, float[] mvpMatrix) {
         // Elapsed is a monotonically increasing time.
         float theta = particle.getOrient(globalT);
         particle.getPosition(globalT, scratch);
@@ -66,9 +148,13 @@ public class SimpleParticleSystem extends ParticleSystem {
         Matrix.setIdentityM( mMat, 0 );
         Matrix.translateM( mMat, 0, scratch.x, scratch.y, scratch.z );
         Matrix.rotateM(mMat, 0, theta, 0, 0, 1);
-        Matrix.multiplyMM( mMat, 0, mMVPMatrix, 0, mMat, 0 );
+        Matrix.multiplyMM( mMat, 0, mvpMatrix, 0, mMat, 0 );
 
-        mParticle.draw(mMat);
+        int mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMat, 0);
+
+        // Draw the quad
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, vertexCount);
     }
 
     @Override
@@ -76,7 +162,7 @@ public class SimpleParticleSystem extends ParticleSystem {
         mPose.set(x, y, z);
         float maxSpeed = 0.0025f;
         synchronized (mSync) {
-            for (int i = 0; i < 5; ++i) {
+            for (int i = 0; i < 10; ++i) {
                 float orient = random.nextFloat() * 360;
                 mVel.set((random.nextFloat() * 2 - 1) * maxSpeed,
                         (random.nextFloat() * 2 - 1) * maxSpeed,
@@ -91,7 +177,6 @@ public class SimpleParticleSystem extends ParticleSystem {
     private Vector3 mVel;
     private Vector3 scratch;
     private float mMat[];
-    private Particle mParticle;
     private Random random;
     private Object mSync;
 
