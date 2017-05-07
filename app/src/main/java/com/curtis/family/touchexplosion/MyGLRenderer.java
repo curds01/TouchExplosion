260d290d524586f8f1f9e605409f32ad6ae785d6
Sean Curtis <Sean Curtis>
2017-05-06 23:02:06 -0700

    Add touch processing
    The quad gets repositioned based on where the user touches. The code needs
    to be refactored, but the basic code is in place for placing the object
    at some arbitrary distance along a line passing through the camera position
    and the point on the screen tapped.

--- app/src/main/java/com/curtis/family/touchexplosion/MyGLRenderer.java
+++ app/src/main/java/com/curtis/family/touchexplosion/MyGLRenderer.java
@@ -3,6 +3,7 @@ package com.curtis.family.touchexplosion;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.view.MotionEvent;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -17,13 +18,26 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     private final float[] mProjectionMatrix = new float[16];
     private final float[] mViewMatrix = new float[16];
 
+    // Window dimensions in pixels
+    private int _wWidth;
+    private int _wHeight;
+
+    private float mX;
+    private float mY;
+    private float mZ;
+    private float mMat[];
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         mParticle = new Particle();
+        mMat = new float[16];
+        mX = mY = mZ = 0.0f;
     }
 
     public void onSurfaceChanged(GL10 unused, int width, int height) {
+        _wWidth = width;
+        _wHeight = height;
+
         GLES20.glViewport(0, 0, width, height);
 
         float ratio = (float) width / height;
@@ -32,7 +46,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         // in the onDrawFrame() method
         Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 0.5f, 7);
         // Set the camera position (View matrix)
-        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, 3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
         // Calculate the projection and view transformation
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
     }
@@ -40,7 +54,12 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        mParticle.draw(mMVPMatrix);
+
+        Matrix.setIdentityM( mMat, 0 );
+        Matrix.translateM( mMat, 0, mX, mY, mZ );
+        Matrix.multiplyMM( mMat, 0, mMVPMatrix, 0, mMat, 0 );
+
+        mParticle.draw(mMat);
 
     public static int loadShader(int type, String shaderCode){
 
@@ -53,5 +72,31 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         GLES20.glCompileShader(shader);
 
         return shader;
+    }
+
+    synchronized public boolean handleTouchEvent(MotionEvent e) {
+        String TAG = "MyGLRenderer";
+        if (e.getActionMasked() == MotionEvent.ACTION_DOWN) {
+
+            // First compute an (x, y) value in canonical space [-1, -1] X [1, 1]
+            float x = 2 * (e.getX() / (float) _wWidth - 0.5f);
+            float y = 2 * ((_wHeight - e.getY()) / (float) _wHeight - 0.5f);
+
+            float depth = 0.1f;
+            // Near, far, and width of the front plane.
+            float n = 0.5f;
+            float f = 7.0f;
+            float e_z = 3.0f;
+            float dist = n + ((depth - 1.0f) * n + depth * f);
+            mZ = e_z - dist;
+            float s = dist / n;
+            float h = s;
+            float w = s * _wWidth / _wHeight;
+            mX = x * w;
+            mY = y * h;
+
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/curtis/family/touchexplosion/MyGLSurfaceView.java
+++ app/src/main/java/com/curtis/family/touchexplosion/MyGLSurfaceView.java
@@ -3,6 +3,7 @@ package com.curtis.family.touchexplosion;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 /**
  * The main GL view in which all interactions are handled.
@@ -25,5 +26,14 @@ public class MyGLSurfaceView extends GLSurfaceView {
         mRenderer = new MyGLRenderer();
         setRenderer(mRenderer);
         setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        boolean handled = mRenderer.handleTouchEvent( e );
+        if ( handled ) {
+            requestRender();
+        }
+        return handled;
     }
 }
